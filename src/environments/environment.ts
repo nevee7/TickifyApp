a9e031a00937357e958b8e57d8989e37f6e47c0d
nevee7 <neve.magnani@gmail.com>
2024-12-14 18:53:44 +0200

    first commit - created the enviroment.ts file so we can integrate the firebase in our angular project

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyB57Ej5FE9K0YK5AWVryqBZuZkMB368zG8",
+    authDomain: "tickify-b05df.firebaseapp.com",
+    projectId: "tickify-b05df",
+    storageBucket: "tickify-b05df.firebasestorage.app",
+    messagingSenderId: "260303438779",
+    appId: "1:260303438779:web:524e78282c02feeff67a9e",
+    measurementId: "G-D9DTD4GXSD",
+  },
+};
